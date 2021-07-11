5a9077c4a58f14dfb5e0b284058d98d4e87e87b5
tuan220301 <84058817+tuan220301@users.noreply.github.com>
2021-07-11 17:33:08 +0700

    Update index.js

--- index.js
+++ index.js
@@ -1,41 +1,36 @@
 window.onload = function(){
     var seconds = 0;
     var tens = 0;
-    var appendTens = document.getElementById("tens");
-    var appendSeconds = document.getElementById("seconds");
     var interval;
-    var btnStart = document.getElementById("start");
-    var btnStop = document.getElementById("stop");
-    var btnReset = document.getElementById("reset");
 
-    btnStart.onclick = function(){
+    document.getElementById("start").onclick = function(){
         clearInterval(interval);
         interval = setInterval(timeStart,10);
     }
-    btnStop.onclick = function(){
+    document.getElementById("stop").onclick = function(){
         clearInterval(interval);
     }
-    btnReset.onclick = function(){
+    document.getElementById("reset").onclick = function(){
         clearInterval(interval);
         tens = 0;
         seconds = 0;
-        appendTens.innerHTML = tens;
-        appendSeconds.innerHTML = seconds;
+        document.getElementById("tens").innerHTML = tens;
+        document.getElementById("seconds").innerHTML = seconds;
     }
     function timeStart(){
         tens++;
         if(tens <= 99){
-            appendTens.innerHTML = tens;
+            document.getElementById("tens").innerHTML = tens;
         }
         if(tens > 99){
             console.log("seconds");
             seconds++;
-            appendSeconds.innerHTML = seconds;
+            document.getElementById("seconds").innerHTML = seconds;
             tens = 0;
-            appendTens.innerHTML = 0;
+            document.getElementById("tens").innerHTML = 0;
         }
         if(seconds > 9){
-            appendSeconds.innerHTM = seconds;
+            document.getElementById("seconds").innerHTM = seconds;
         }
     }
 }
